aa60b51127176459d0fee4bbf31ca721ebebaa62
perrylzx <46482518+perrylzx@users.noreply.github.com>
2020-02-20 16:58:30 +0800

    fix merge problems

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -1,14 +1,7 @@
 let nextTodoId = 0
 
-<<<<<<< HEAD
 export const addTodo = value => ({
 
-=======
-export const addTodo = text => ({
-  type: 'ADD_TODO',
-  id: nextTodoId++,
-  text
->>>>>>> fd408eb3792f0ee169f614b41b7f465c4f3274b5
 })
 
 export const setVisibilityFilter = filter => ({
@@ -25,30 +18,17 @@ export const requestTodos = () => ({
   type: 'REQUEST_TODOS',
 })
 
-<<<<<<< HEAD
 export const receiveTodos = (todos) => ({
   type: 'RECEIVE_TODOS',
   todos,
-=======
-export const receiveTodos = (json) => ({
-  type: 'RECEIVE_TODOS',
-  value: json.data.children.map(child => child.data),
->>>>>>> fd408eb3792f0ee169f614b41b7f465c4f3274b5
 })
 
 export const fetchTodo = () => dispatch => {
   dispatch(requestTodos());
-<<<<<<< HEAD
-  console.log('def')
   return fetch('http://localhost:4000/request-todo')
     .then(response => {
-      console.log('ghi')
       return response.json();
     })
-=======
-  return fetch('http://localhost:4000/request-todos')
-    .then(response => response.json())
->>>>>>> fd408eb3792f0ee169f614b41b7f465c4f3274b5
     .then(json => dispatch(receiveTodos(json)))
 }
 
