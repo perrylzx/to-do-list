6bf406b20da51ec64d3b2e47ca1d9c219e40ee3f
perrylzx <46482518+perrylzx@users.noreply.github.com>
2020-02-20 16:54:53 +0800

    merge fix 3

--- frontend/src/reducers/todos.js
+++ frontend/src/reducers/todos.js
@@ -12,29 +12,19 @@ const todos = (state = initialState, action) => {
           ...state.todos,
           {
             id: action.id,
-<<<<<<< HEAD
             value: action.value,
-=======
             text: action.text,
 >>>>>>> fd408eb3792f0ee169f614b41b7f465c4f3274b5
             completed: false
           }
         ]
       }
-<<<<<<< HEAD
     case 'REQUEST_TODOS':
-=======
-    case 'REQUEST_TODO':
->>>>>>> fd408eb3792f0ee169f614b41b7f465c4f3274b5
       return {
         ...state,
         isFetching: true,
       }
-<<<<<<< HEAD
     case 'RECEIVE_TODOS':
-=======
-    case 'RECEIVE_TODO':
->>>>>>> fd408eb3792f0ee169f614b41b7f465c4f3274b5
       return {
         ...state,
         isFetching: false,
@@ -52,17 +42,4 @@ const todos = (state = initialState, action) => {
   }
 }
 
-<<<<<<< HEAD
 export default todos
-=======
-
-
-//       return state.map(todo =>
-//         todo.id === action.id ? { ...todo, completed: !todo.completed } : todo
-//       )
-//     default:
-//       return state
-//   }
-// }
-export default todos
->>>>>>> fd408eb3792f0ee169f614b41b7f465c4f3274b5
